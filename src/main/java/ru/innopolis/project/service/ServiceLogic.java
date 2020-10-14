add97d4ffe1b83f80ea41b9cab67fa9d303cb4e3
AlexeyAldoninSTC <a.aldonin.stc@innopolis.university>
2020-10-14 15:31:10 +0300

    +Service Logic

--- src/main/java/ru/innopolis/project/service/ServiceLogic.java
+++ src/main/java/ru/innopolis/project/service/ServiceLogic.java
@@ -1,9 +1,7 @@
 package ru.innopolis.project.service;
 
-
 import java.util.Map;
 
 public interface ServiceLogic {
-
     Map<String, Boolean> execute(String[] rules, Map<String, Integer> features);
 }
