6e43e16047a068f69ba018411d1be3cbd891bced
AlexeyAldoninSTC <a.aldonin.stc@innopolis.university>
2020-10-21 13:13:15 +0300

    rules admin interface

---
+++ response-generator/src/main/java/ru/innopolis/project/controllers/AdminRestController.java
@@ -0,0 +1,35 @@
+package ru.innopolis.project.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.innopolis.project.entity.Rule;
+import ru.innopolis.project.repositories.RulesRepository;
+
+@RestController(value = "/api/admin")
+public class AdminRestController {
+
+    private final RulesRepository rulesRepository;
+
+    @Autowired
+    public AdminRestController(RulesRepository rulesRepository) {
+        this.rulesRepository = rulesRepository;
+    }
+
+    @GetMapping(value = "/getRule/{id}")
+    public Rule getRule(@PathVariable("id") Long id) {
+        return id == 0 ? new Rule() : getRule(id);
+    }
+
+    @Transactional
+    @PostMapping()
+    public Rule saveRule(Rule rule){
+        return rulesRepository.save(rule);
+    }
+
+
+
+}
--- response-generator/src/main/java/ru/innopolis/project/repositories/RulesRepository.java
+++ response-generator/src/main/java/ru/innopolis/project/repositories/RulesRepository.java
@@ -1,8 +1,10 @@
 package ru.innopolis.project.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import ru.innopolis.project.entity.Rule;
 
+@Repository
 public interface RulesRepository extends JpaRepository<Rule, Long> {
 
     Rule findByName(String name);
